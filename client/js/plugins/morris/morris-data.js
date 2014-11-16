a3326b95fad492f5b269e30aaec2c2a6816aba32
yguoo <xguo2008@gmail.com>
2014-11-16 09:56:55 -0500

    graph axix change

--- client/js/plugins/morris/morris-data.js
+++ client/js/plugins/morris/morris-data.js
@@ -3,61 +3,61 @@ $(function() {
      new Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010',
+            period: '0',
             algebra: 2666,
             geometry: null,
             stat: 2647,
             calculus: 2647
         }, {
-            period: '2011',
+            period: '1',
             algebra: 2778,
             geometry: 2294,
             stat: 2441,
             calculus: 2647
         }, {
-            period: '2012',
+            period: '2',
             algebra: 4912,
             geometry: 1969,
             stat: 2501,
             calculus: 2647
         }, {
-            period: '2013',
+            period: '3',
             algebra: 3767,
             geometry: 3597,
             stat: 5689,
             calculus: 2647
         }, {
-            period: '2014',
+            period: '4',
             algebra: 6810,
             geometry: 1914,
             stat: 2293,
             calculus: 2647
         }, {
-            period: '2015',
+            period: '5',
             algebra: 5670,
             geometry: 4293,
             stat: 1881,
             calculus: 2647
         }, {
-            period: '2016',
+            period: '6',
             algebra: 4820,
             geometry: 3795,
             stat: 1588,
             calculus: 2647
         }, {
-            period: '2017',
+            period: '7',
             algebra: 15073,
             geometry: 5967,
             stat: 5175,
             calculus: 2647
         }, {
-            period: '2018',
+            period: '8',
             algebra: 10687,
             geometry: 4460,
             stat: 2028,
             calculus: 2647
         }, {
-            period: '2019',
+            period: '9',
             algebra: 8432,
             geometry: 5713,
             stat: 1791,
@@ -69,7 +69,8 @@ $(function() {
         pointSize: 2,
         hideHover: 'auto',
         resize: true,
-        lineColors: ['#428bca','#5cb85c','#f0ad4e','#d9534f']
+        lineColors: ['#428bca','#5cb85c','#f0ad4e','#d9534f'],
+        xLabelFormat : function(x){return x.getFullYear() - 1900;}
     });
 
 
