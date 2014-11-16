a6ff83d4f5e838fa4d029fc957f1c8a08ec2bde1
Keri Warr <keri@warr.ca>
2014-11-15 19:52:58 -0500

    question model update

--- server/api/question/question.model.js
+++ server/api/question/question.model.js
@@ -4,9 +4,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var QuestionSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  text: { type: String, required: true },
+  answer: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
